ec23249f4cc84a8176355a8e868f764d8ce3b0d9
Jonsrn <jonathansrn2@gmail.com>
2024-11-08 02:04:20 -0300

    Add files via upload

---
+++ Bibliografia/Menus.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include "bibliografia.h"
+
+void menu_principal(){
+    printf("Bem vindo a Bibliografia\n"); 
+    printf("[1]-Importar Bibliografia\n"); 
+    printf("[2]-Realizar Atividades\n"); 
+    printf("[3]-Sobre\n"); 
+    printf("[0]-Sair\n"); 
+} 
+
+void sobre(){
+    printf("Trabalho realizado por: Jonathan dos Santos Barbosa\n"); 
+}
+
+void realizar_atividades(){
+    printf("[1]-Imprimir todas as palavras em português seguida das suas equivalentes em inglês, de uma unidade específica\n"); 
+    printf("[2]-Imprimir todas as palavras em ingles, correspondentes a palavra em português digitada\n"); 
+    printf("[3]-Remover palavra em inglês, informando a palavra em inglês e a unidade na qual ela pertence\n"); 
+    printf("[4]-Remover pa\n"); 
+    printf("[0]-Sair\n"); 
+}
+
+/*
+informar uma palavra em português e a unidade a qual a mesma pertence e então remove-la, para isto 
+deve remover a palavra em inglês da árvore binária correspondente a palavra em português da mesma 
+unidade. Caso ela seja a única palavra na árvore binária, a palavra em português deve ser removida da 
+árvore 2-3. 
+
+*/
---
+++ Bibliografia/bibliografia.h
@@ -0,0 +1,101 @@
+#ifndef _BIBLIOGRAFIA_H
+#define _BIBLIOGRAFIA_H
+
+//Prototipos das estruturas
+
+typedef struct Lista_Unidades list_unid; //prototipo da lista encadeada das unidades
+typedef struct Infos_exclusao inf_ex; //Essa estrutura aqui é pra armazenar as variaveis de exclusao
+typedef struct InfoArvBB InfoBB;    
+typedef struct ArvBB_ingles ArvBB_ing; //prototipo da arvore BB, pra colocar as palavras em ingles
+typedef struct Info_Arv_23 InfArv23;  //prototipo da informação da arvore 2-3
+typedef struct ARV_23_Portugues Arv23Port; //prototipo da arvore 2-3
+
+typedef struct Lista_Unidades{
+    int unidade;
+    list_unid *prox; 
+}list_unid;
+
+typedef struct Infos_exclusao{
+    char palavra_ser_excluida[100]; 
+    int unidade; 
+}inf_ex;
+
+typedef struct InfoArvBB{
+    char palavra_ingles[100]; 
+}InfoBB; 
+
+typedef struct ArvBB_ingles{
+    InfoBB info; 
+    list_unid *unidades; 
+    ArvBB_ing *esq, *dir; 
+}ArvBB_ing;
+
+
+typedef struct Info_Arv_23{
+    char palavra_portugues[100]; 
+    ArvBB_ing *significados_ingles; 
+}InfArv23; 
+
+
+typedef struct ARV_23_Portugues{
+    InfArv23 info1, info2;
+    int n_infos; 
+    Arv23Port *esq, *cen, *dir; 
+}Arv23Port;
+
+//Protótipos dos Menus 
+
+
+void menu_principal(); 
+void sobre(); 
+void realizar_atividades(); 
+
+//Protótipo das Funções auxiliares
+
+
+
+
+
+//Protótipos das funções da árvore 2-3
+
+
+
+
+
+//Protótipos das funções da árvore Binária
+
+ArvBB_ing* criar_no_ArvBB(InfoBB info); //Função de Criação do Nó. 
+int inserir_ArvBB_Ingles(ArvBB_ing **Raiz, ArvBB_ing *novo_no, ArvBB_ing **no_existente); //Essa função insere o novo Nó na árvore, e recupera o endereço de seu Nó na árvore, mas se houver o Nó existente na árvore, seu Nó original será recuperado e o novo descartado. 
+int Armazenar_No_ARVBB(ArvBB_ing *Raiz, int unidade, ArvBB_ing ***vetor_ingles, int *tam_vetor); // Função que recupera os Nós correspondentes, pra impressão posterior.
+int menor_filho(ArvBB_ing *Raiz, ArvBB_ing **menor); //Função que vai recuperar o endereço do menor filho à direita
+int remover_No_ArvBB(ArvBB_ing **Raiz, inf_ex informacoes); //Função que vai excluir a palavra em ingles da arvore, caso a unidade corresponda E a lista seja nula
+
+
+
+//Protótipo das funções da Lista Encadeada
+int criar_no_Lista(list_unid **novo_elemento, int unidade); //Função pra criar o Nó da Lista
+int inserir_na_Lista(list_unid **Raiz, list_unid **novo_elemento); //Função para inserir na Lista a Unidade
+int buscando_unidade(list_unid *Raiz_Lista, int unidade_buscada); //Função de percorrer a Lista, em busca da unidade pesquisada. 
+int remover_unidade_lista(list_unid **Raiz_Lista, int unidade_buscada); // Função para remover a Unidade da Lista. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
