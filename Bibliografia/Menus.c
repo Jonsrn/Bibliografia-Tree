2d726d1b5f5f2b5e6f5ab83f573b771c56ce3263
Jonsrn <jonathansrn2@gmail.com>
2024-11-09 02:34:48 -0300

    Update Menus.c

--- Bibliografia/Menus.c
+++ Bibliografia/Menus.c
@@ -1,14 +1,23 @@
 #include <stdio.h> 
 #include <stdlib.h> 
+#include <string.h>
 #include "bibliografia.h"
 
 void menu_principal(){
     printf("Bem vindo a Bibliografia\n"); 
     printf("[1]-Importar Bibliografia\n"); 
     printf("[2]-Realizar Atividades\n"); 
-    printf("[3]-Sobre\n"); 
+    printf("[3]-Testes de Funcionalidades Base\n"); 
+    printf("[4]-Sobre\n"); 
     printf("[0]-Sair\n"); 
 } 
+
+void testes_menu(){
+    printf("[1]-Testar Arvore 2-3 Individual\n"); 
+    printf("[2]-Testar Arvore BB Individual\n"); 
+    printf("[3]-Teste da Lista Encadeada Individual\n"); 
+    printf("[4]-Teste das 3 estruturas de maneira Integrada\n"); 
+}
 
 void sobre(){
     printf("Trabalho realizado por: Jonathan dos Santos Barbosa\n"); 
