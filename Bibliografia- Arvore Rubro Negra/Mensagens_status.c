49f5580c1b15dff1797fe5efb2445a2838a69fa0
Jonsrn <jonathansrn2@gmail.com>
2024-11-29 01:35:32 -0300

    Update Mensagens_status.c

--- Bibliografia- Arvore Rubro Negra/Mensagens_status.c
+++ Bibliografia- Arvore Rubro Negra/Mensagens_status.c
@@ -3,7 +3,29 @@
 
 
 //função de diagnostico da importação do dataset
-
+void mensagem_status_montagem_dataset(int situacao){
+    if(situacao == 1){
+        printf("A operação de preenchimento do Dataset realizada com sucesso.\n"); 
+    }
+    if(situacao == 2){
+        printf("A operação falhou porque houve um problema na alocação do nó da lista.\n"); 
+    }
+    if(situacao == 3){
+        printf("A operação falhou porque a palavra em português, seu significado em inglês e assim como sua unidade já estão presentes no dataset.\n"); 
+    }
+    if(situacao == 4){
+        printf("A operação falhou porque houve um problema na alocação do nó da Árvore Binária.\n"); 
+    }
+    if(situacao == 5){
+        printf("A operação falhou porque houve um problema na Alocação do Nó da 2-3.\n"); 
+    }
+    if(situacao == 6){
+        printf("A operação falhou porque o Dataset já foi preenchido.\n"); 
+    }
+    if(situacao == 7){
+        printf("A operação falhou porque não foi possivel abrir o arquivo.\n"); 
+    }    
+}
 
 
 
@@ -12,27 +34,28 @@
 void mensagem_status_impressao_unidade(int situacao){
 
     if(situacao == 0){
-        printf("A operação falhou, porque a árvore está vazia\n"); 
+        printf("A operação falhou, porque a árvore está vazia.\n"); 
     }
     if(situacao == 1){
-        printf("A operacao foi realizada com sucesso\n"); 
+        printf("A operacao foi realizada com sucesso.\n"); 
 
     }
     if(situacao == 2){
-        printf("Nenhum correspondente foi encontrado com o valor pesquisado\n"); 
+        printf("Nenhuma palavra correspondente foi encontrada com o valor pesquisado.\n"); 
     }
+  
 }
 
 void mensagem_status_impressao_palavra_especifica(int situacao){
 
     if(situacao == 0){
-        printf("A operação falhou, por que a árvore está vazia\n");
+        printf("A operação falhou, por que a árvore está vazia.\n");
     }
     if(situacao == 1){
-        printf("A operação foi realizada com sucesso \n");
+        printf("A operação foi realizada com sucesso.\n");
     }
     if(situacao == 2){
-        printf("A operação falhou, porque a palavra pesquisada não foi encontrada\n"); 
+        printf("A operação falhou, porque a palavra pesquisada não foi encontrada.\n"); 
     }
 }
 
@@ -41,13 +64,13 @@ void mensagem_status_impressao_palavra_especifica(int situacao){
 
 void mensagem_status_exclusao_ingles_unidade(int situacao){
     if(situacao == 0){
-         printf("A operação falhou, por que a árvore está vazia\n");
+         printf("A operação falhou, por que a árvore está vazia.\n");
     }
     if(situacao == 1){
-        printf("A operação foi realizada com sucesso \n");
+        printf("A operação foi realizada com sucesso.\n");
     }
     if(situacao == 2){
-
+        printf("A operação falhou, porque nenhuma palavra com a unidade pesquisada foi encontrada.\n");
     }
 }
 
@@ -58,12 +81,21 @@ void mensagem_status_exclusao_ingles_unidade(int situacao){
 void mensagem_status_exclusao_correspondente_ingles(int situacao){
 
     if(situacao == 0){
-         printf("A operação falhou, por que a árvore está vazia\n");
+         printf("A operação falhou, por que a árvore está vazia.\n");
     }
     if(situacao == 1){
-         printf("A operação foi realizada com sucesso \n");
+         printf("A operação foi realizada com sucesso, palavra em português removida da árvore RN.\n");
     }
     if(situacao == 2){
-        printf("Nenhuma palavra com a unidade pesquisada foi encontrada.\n"); 
+        printf("A operação falhou porque nenhuma palavra em português correspondente foi encontrada.\n"); 
+    }
+    if(situacao == 3){
+        printf("A operação foi realizada com sucesso, as palavra em inglês correspondentes a unidade foram removidas.\n");
+    }
+    if(situacao == 4){
+        printf("A operação falhou porque a remoção da palavra em português na Rubro Negra não foi bem sucedida.\n");
+    }
+    if(situacao == 5){
+        printf("A operação falhou porque a remoção não encontrou nenhuma palavra em inglês correspondente a unidade solicitada.\n");
     }
 }
